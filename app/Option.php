60dd4838075989b8cdc6e462596335f0cbbc122d
Phan Thanh Dong <phanthanhdongtd@gmail.com>
2018-12-17 18:50:38 +0700

    remove softdelete

--- app/Option.php
+++ app/Option.php
@@ -3,11 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Option extends Model
 {
-    use SoftDeletes;
     protected $fillable=[
     	'name','option_group_id'
     ];
--- app/Product_option.php
+++ app/Product_option.php
@@ -3,10 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 class Product_option extends Model
 {
-    use SoftDeletes;
       protected $fillable=[
     	'price_increase','product_id','option_id'
     ];
